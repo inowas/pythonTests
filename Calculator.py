72fc540629543e7471153af5f339a143486705de
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-10-14 09:36:32 +0200

    Update Calculator

--- Calculator.py
+++ Calculator.py
@@ -5,3 +5,13 @@ class Calculator:
 
     def sum(self, a, b):
         return a+b
+
+    def multiply(self, a, b=None):
+        if b is None:
+            return a*a
+
+        return a*b
+
+    def poweroftwo(self, value):
+        return 2 ** value
+
--- CalculatorTest.py
+++ CalculatorTest.py
@@ -2,7 +2,24 @@ import unittest
 from Calculator import Calculator
 
 
-class SumTest(unittest.TestCase):
-    def test(self):
+class CalculatorTest(unittest.TestCase):
+    def test_SumWithTwoParameterTest(self):
         calculator = Calculator()
         self.assertEqual(5, calculator.sum(2, 3))
+
+    def test_MultiplicationWithTwoParameterTest(self):
+        calculator = Calculator()
+        self.assertEqual(6, calculator.multiply(2, 3))
+        self.assertEqual(6, calculator.multiply(3, 2))
+
+    def test_MultiplicationWithOneParameterReturnSquareTest(self):
+        calculator = Calculator()
+        self.assertEqual(9, calculator.multiply(3))
+
+    def test_powerof_two(self):
+        calculator = Calculator()
+        self.assertEqual(8, calculator.poweroftwo(3))
+
+
+if __name__ == '__main__':
+    unittest.main()
